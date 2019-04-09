4df1b53e77a2ef994b1cc93ee5dcda4ec73ec9e6
Furkan Emre <furkan54emre@gmail.com>
2019-04-09 22:50:03 +0300

    Menu Navigaition working properly in 5 menu content:introduced

--- src/views/ComponentCommunicator.java
+++ src/views/ComponentCommunicator.java
@@ -1,10 +1,19 @@
 package views;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javafx.scene.layout.Pane;
 import views.container.ComponentContainerController;
 import views.container.ContainerPosition;
+import views.menucontent.coursemanagement.managecourse.ManageCourseController;
+import views.menucontent.coursemanagement.managecourse.TestManageCourse;
+import views.menucontent.coursemanagement.schedulecourses.ScheduleCoursesController;
+import views.menucontent.coursemanagement.schedulecourses.TestScheduleCourses;
+import views.menucontent.notificationmanagement.announcement.AnnouncementController;
+import views.menucontent.notificationmanagement.announcement.TestAnnouncement;
+import views.menucontent.notificationmanagement.manageemails.ManageEmailsController;
+import views.menucontent.notificationmanagement.manageemails.TestManageEmails;
 import views.menucontent.usermanagement.UserManagementController;
 
 
@@ -15,14 +24,44 @@ public class ComponentCommunicator implements IMenuCommunicator {
 	// Pop-up reference can be put here
 	
 	public ComponentCommunicator() {
-		
+		menuContentList = new ArrayList<Pane>();
 	}
 	
-	public void updateSelection(String selection) {
-		if(selection.equals("userManagement")) {
-			container.addComponent(new UserManagementController(), ContainerPosition.CENTER);
+	public void updateSelection(String menuContentID) {
+		//TODO How, we can improve it. Using state design pattern
+		//TODO Only testing purposes later builder or factories will be introduced
+		
+		Pane searchResult = searchFor(menuContentID);
+		
+		if(searchResult == null) {
+			if(menuContentID.equals("userManagement")) {
+				searchResult = new UserManagementController();
+			}
+			else if(menuContentID.equals("manageEmails")) {
+				searchResult = (Pane) new TestManageEmails().build();
+			}
+			else if(menuContentID.equals("announcements")) {
+				searchResult = (Pane) new TestAnnouncement().build();
+			}
+			else if(menuContentID.equals("manageCourse")) {
+				searchResult = (Pane) new TestManageCourse().build();
+			}
+			else if(menuContentID.equals("scheduleCourse")) {
+				searchResult = (Pane) new TestScheduleCourses().build();
+			}
+			else {
+				//TODO It can be exception
+				System.out.println("Please define your menu in updateSelection");
+			}
+			
+			if(searchResult != null) {
+				menuContentList.add(searchResult);
+			}
 		}
+		
+		container.addComponent(searchResult, ContainerPosition.CENTER);
 	}
+	
 	
 	public void addMenuContent(Pane menuContent) {
 		
@@ -32,8 +71,15 @@ public class ComponentCommunicator implements IMenuCommunicator {
 		
 	}
 	
-	private void updateContainerMenuContent() {
+	private Pane searchFor(String menuID) {
+		for (Pane pane : menuContentList) {
+			String id = pane.getId();
+			if(id.equals(menuID)) {
+				return pane;
+			}
+		}
 		
+		return null;
 	}
 	
 	// Dependency makes me sick, sorry
---
+++ src/views/menucontent/MenuContent.java
@@ -0,0 +1,8 @@
+package views.menucontent;
+
+public enum MenuContent {
+	USER_MANAGEMENT,
+	MANAGE_EMAIL,
+	ANNOUNCEMENT
+
+}
