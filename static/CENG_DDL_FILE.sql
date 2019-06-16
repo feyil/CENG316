acba84e951701ddfe973907cf2e25e02964b5390
Furkan Emre <furkan54emre@gmail.com>
2019-06-16 16:24:00 +0300

    Email Tables added to DDL

--- static/CENG_DDL_FILE.sql
+++ static/CENG_DDL_FILE.sql
@@ -13,3 +13,21 @@ CREATE TABLE USER(
     
 INSERT INTO USER(UEMAIL, UPASSWORD,UTYPE, UNAMESURNAME, UTITLE)
 VALUES ('app@ceng.com', '$2a$05$77azOIF/ZCDBVhIHPLZhseNg/H6nsMjcAIZ1NgAoV/YkN0GDo4N.e', 'Admin', 'App CENG', 'App');
+
+create table EMAIL_MEMBER(
+	ID int not null auto_increment,
+    NAMESURNAME varchar(30) not null,
+    EMAIL varchar(30) not null unique,
+    GRADE int not null,
+    primary key(ID));
+    
+create table EMAIL_MEMBER_GROUP(
+	ID int not null auto_increment,
+    MEMBER_ID int not null,
+    GROUP_ID int not null,
+    primary key(ID));
+    
+create table EMAIL_GROUP(
+	ID int not null auto_increment,
+    NAME varchar(30) not null unique,
+    primary key(ID));
